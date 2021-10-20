961240c7de069565dd4b279187897a31e58bc642
waqas-ahmd <65waqas@gmail.com>
2021-10-20 17:41:09 +0500

    updated

---
+++ index.js
@@ -0,0 +1,54 @@
+const videoBox = document.querySelector("#video");
+const buttonsBox = document.querySelector(".buttonsBox");
+
+var videoWidth = 650;
+var videoHeight = 500;
+var videoIndex = 0;
+
+if (window.innerWidth < 650) {
+  videoBox.width = window.innerWidth;
+  videoBox.height = (videoBox.width * 500) / 650;
+  videoHeight = videoBox.height;
+  videoWidth = videoBox.width;
+}
+
+videoBox.src = `//www.pkcast123.me/embed.php?player=desktop&live=${channelList[videoIndex].tvId}&vw=${videoWidth}&vh=${videoHeight}`;
+
+channelList.forEach((channel, index) => {
+  let innerH = buttonsBox.innerHTML;
+  innerH += `<button class="channel-btn" id="cb-${index}" >${channel.title}</button>`;
+  buttonsBox.innerHTML = innerH;
+});
+
+const channelButtons = document.querySelectorAll(".channel-btn");
+channelButtons[0].disabled = true;
+
+videoBox.addEventListener("click", () => {
+  e.preventDefault();
+});
+
+channelButtons.forEach((channelButton, index) => {
+  channelButton.addEventListener("click", () => {
+    videoIndex = index;
+    videoBox.src = `//www.pkcast123.me/embed.php?player=desktop&live=${channelList[videoIndex].tvId}&vw=${videoWidth}&vh=${videoHeight}`;
+    channelButton.disabled = true;
+    channelButtons.forEach((button, idx) => {
+      if (idx !== index) button.disabled = false;
+    });
+  });
+});
+
+// window.onresize = () => {
+//   if (window.innerWidth < 650) {
+//     videoBox.width = window.innerWidth;
+//     videoBox.height = (videoBox.width * 500) / 650;
+//     videoHeight = videoBox.height;
+//     videoWidth = videoBox.width;
+//   } else {
+//     videoBox.width = 650;
+//     videoBox.height = (videoBox.width * 500) / 650;
+//     videoHeight = videoBox.height;
+//     videoWidth = videoBox.width;
+//   }
+//   videoBox.src = `//www.pkcast123.me/embed.php?player=desktop&live=${channelList[videoIndex].tvId}&vw=${videoWidth}&vh=${videoHeight}`;
+// };
---
+++ utils.js
@@ -0,0 +1,42 @@
+const channelList = [
+  {
+    title: "PTV Sports",
+    link: "//stream.crichd.vip/update/ptv.php",
+    tvId: "ptvpk",
+  },
+  {
+    title: "Ten Sports",
+    link: "//stream.crichd.vip/update/tensp.php",
+    tvId: "tenspk",
+  },
+  {
+    title: "Willow HD",
+    link: "//stream.crichd.vip/update/willowcricket.php",
+    tvId: "willowusa",
+  },
+  {
+    title: "Sky Sports",
+    link: "//stream.crichd.vip/update/skys2.php",
+    tvId: "skyscric",
+  },
+  {
+    title: "Star Sports",
+    link: "//stream.crichd.vip/update/star.php",
+    tvId: "star1in",
+  },
+  {
+    title: "Star Hindi",
+    link: "//stream.crichd.vip/update/star1hi.php",
+    tvId: "starhindi",
+  },
+  {
+    title: "Channel 3",
+    link: "//stream.crichd.vip/update/vh3.php",
+    tvId: "hdchnl3",
+  },
+  {
+    title: "Channel 6",
+    link: "//stream.crichd.vip/update/ch6.php",
+    tvId: "hdchnl6",
+  },
+];
